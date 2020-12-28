20ea39db89b7f5ab0422f4cdc75b8c8e0f88f712
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-12-28 13:00:25 -0600

    Initial commit. Video 4. Made first pages

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function FourOhFourPage() {
+    return (
+        <p>This page does not exist</p>
+    );
+}
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SlicemastersPage() {
+    return (
+        <div>
+            <p>This is the Slicemasters Page!</p>
+        </div>
+    );
+}
