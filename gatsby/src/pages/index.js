0a015f4828e892374900c6e5b36b378edbf67934
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-12-28 13:17:49 -0600

    Video 5. Routing and Navigation in Gatsby. Started creating the Nav component. We are using Link tag instead of html anchor tag because of the behind the scenes javascript that it includes, which for example, does not reload the whole page.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function HomePage() {
     return (
         <div>
+            <Nav />
             <p>This is the Home Page!</p>
         </div>
     );
