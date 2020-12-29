dcdf8dde6ebb6e4fb4569b9b5834e63fb77f026c
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-12-28 19:03:32 -0600

    Video 6. Created a global layout using gatsby-browser.js and gatsby-ssr.js

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function HomePage() {
     return (
-        <div>
-            <Nav />
-            <p>This is the Home Page!</p>
-        </div>
+        <>
+            <p>Home Page</p>
+        </>
     );
 }
