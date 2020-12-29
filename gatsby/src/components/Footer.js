e38cfc3aab909d3eff2b3c19e025180a8242f32f
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-12-29 13:04:19 -0600

    Video 7. Added Global Styles file and imported it in Layout.js

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer>
-            <p>&copy; Slick's Slices {new Date().getFullYear}</p>
+            <p>Footer content</p>
         </footer>
     );
 }
