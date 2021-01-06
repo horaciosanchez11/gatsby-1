a0edfb63a32c582ec3c016f1be8518886fa44e92
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2021-01-05 18:50:38 -0600

    Video 24. Styled the Single Pizza template

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,9 +1,28 @@
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
+import styled from 'styled-components';
 
-export default function SinglePizzaPage() {
+const PizzaGrid = styled.div`
+    display:grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+
+`;
+
+export default function SinglePizzaPage({data: {pizza}}) {
     return (
-        <p>Single Pizzas</p>
+        <PizzaGrid>
+            <Img fluid={pizza.image.asset.fluid} />
+            <div>
+                <h2 className="mark">
+                    {pizza.name}
+                </h2>
+                <ul>
+                    {pizza.toppings.map(topping => <li key={topping.id}>{topping.name}</li>)}
+                </ul>
+            </div>
+        </PizzaGrid>
     )
 }
 
