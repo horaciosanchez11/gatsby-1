2f347bc9f02832b394562183153bc4ccec147b94
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2021-01-07 17:17:08 -0600

    Video 34. Started creating the Order page

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,71 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
+import Img from 'gatsby-image';
+import calculatePizzaPrice from '../utils/calculatePizzaPrice';
+import formatMoney from '../utils/formatMoney';
 
-export default function OrderPage() {
+export default function OrderPage( {data} ) {
+    const pizzas = data.pizzas.nodes;
+    const {values, updateValues} = useForm({
+        name: '',
+        email: ''
+    })
+
     return (
         <>
             <SEO title="Order a pizza" />
-            <p>This is the Order Page!</p>
+            <form>
+                <fieldset>
+                    <legend>Your Info</legend>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={values.name} onChange={updateValues}  />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" value={values.email} onChange={updateValues} />
+                </fieldset>
+                <fieldset>
+                    <legend>Menu</legend>
+                    {pizzas.map(pizza => (
+                        <div key={pizza.id}>
+                            <Img width="50" height="50" fluid={pizza.image.asset.fluid } alt={pizza.name}/>
+                            <div>
+                                <h2>{pizza.name}</h2>
+                            </div>
+                            <div>
+                                {['S', 'M', 'L'].map(size => (
+                                    <button key={size} type="button">{size} {formatMoney(calculatePizzaPrice(pizza.price, size))}</button>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </fieldset>
+                <fieldset>
+                    <legend>Order</legend>
+                </fieldset>
+            </form>
         </>
     );
 }
+
+export const query = graphql`
+    query {
+        pizzas: allSanityPizza {
+            nodes {
+                name
+                id
+                slug {
+                    current
+                }
+                price
+                image {
+                    asset {
+                        fluid(maxWidth:100) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
