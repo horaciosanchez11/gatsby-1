e8004d76e73c846e0d57af642174b58d3842fcae
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2021-01-08 16:56:27 -0600

    Video 40. Modified the pizza custom hook to send the order data

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -17,15 +17,18 @@ export default function OrderPage( {data} ) {
         name: '',
         email: ''
     })
-    const {order, addToOrder, removeFromOrder} = usePizza({
+    const {order, addToOrder, removeFromOrder, error, loading, message, submitOrder} = usePizza({
         pizzas, 
-        inputs: values
+        values
     });
 
+    if (message) {
+        return <p>{message}</p>
+    }
     return (
         <>
             <SEO title="Order a pizza" />
-            <OrderStyles>
+            <OrderStyles onSubmit={submitOrder}>
                 <fieldset>
                     <legend>Your Info</legend>
                     <label htmlFor="name">Name</label>
@@ -69,8 +72,11 @@ export default function OrderPage( {data} ) {
                     <h3>
                         Your Total is: {formatMoney(calculateOrderTotal(order, pizzas))}
                     </h3>
-                    <button type="submit">
-                        Order Ahead
+                    <div>
+                        {error ? <p>Error: {error} </p>: ''}
+                    </div>
+                    <button type="submit" disabled={loading} >
+                        {loading ? 'Placing Order...' : 'Order ahead'}
                     </button>
                 </fieldset>
             </OrderStyles>
--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,10 +1,16 @@
 import { useContext, useState } from "react";
 import OrderContext from "../components/OrderContext";
+import attachNamesAndPrices from "./attachNamesAndPrices";
+import calculateOrderTotal from "./calculateOrderTotal";
+import formatMoney from "./formatMoney";
 
-export default function usePizza({pizzas, inputs}) {
+export default function usePizza({pizzas, values}) {
     // 1. create state to hold order
     //const [order, setOrder] = useState([]);
     const [order, setOrder] = useContext(OrderContext);
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState('');
 
     // 2. make function to add pizzas to order
     function addToOrder(orderedPizza) {
@@ -19,10 +25,50 @@ export default function usePizza({pizzas, inputs}) {
         ]);
     }
 
-    // 4. send data to serverless function when they checkout
+    // function that is run when someone submits the place order form
+    async function submitOrder(e) {
+        e.preventDefault();
+        
+        setLoading(true);
+        setError(null);
+        setMessage(null);
+        // gather all the data that needs to be sent
+        const body = {
+            order: attachNamesAndPrices(order, pizzas),
+            total: formatMoney(calculateOrderTotal(order, pizzas)),
+            name: values.name,
+            email: values.email
+        }
+
+        console.log(body);
+
+        // 4. send data to serverless function when they checkout
+        const res = await fetch(`${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'            
+            },
+            body: JSON.stringify(body),
+        });
+
+        const text = JSON.parse(await res.text());
+
+        if (res.status >= 400 && res.status < 600) {
+            setLoading(false);
+            setError(text.message);
+        }  else {
+            setLoading(false);
+            setMessage('Success!');
+        }
+    }    
+
     return {
         order,
         addToOrder,
-        removeFromOrder
+        removeFromOrder,
+        error,
+        loading,
+        message,
+        submitOrder
     }
 }
