d28dbca639509e0ab72c4093d2a3afbde1e0300b
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2021-01-08 19:15:17 -0600

    Video 41. Added validations to the serverless function to place an order, as well as getting the order info and copied it to the mail to be sent

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,25 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({order, total}) {
+    return (
+        `
+            <div>
+                <h2>Your order for ${total}</h2>
+                <p>Please start walking over, we will have your order ready in the next 20 minutes </p>
+                <ul>
+                    ${order.map(item => `
+                        <li>
+                            <img src="${item.thumbnail}" alt="${item.name}" />
+                            ${item.size} ${item.name} - ${item.price}
+                        </li>
+                    `).join('')}
+                </ul>
+                <p>Your total is ${total}</p>
+            </div>
+        `
+    )
+}
+
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
@@ -10,19 +30,29 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-
-
 exports.handler = async(event, context) => {
+    const body = JSON.parse(event.body);
+    // validate data coming in
+    const requiredFields = ['email', 'name', 'order'];
+    for (const field of requiredFields) {
+        if (!body[field]) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({message: `You are missing ${field} field`})
+            }
+        }
+    }
+
     // Test send an email
     const info = await transporter.sendMail({
         from: 'Slick Slices <slick@example.com>',
-        to: 'orders@example.com',
+        to: `${body.name} <${body.email}>, orders@example.com`,
         subject: 'New Order',
-        html: `<p>Ypur new pizza order is here!</p>`
+        html: generateOrderEmail({order: body.order, total: body.total})
     });
 
     return {
         statusCode: 200,
-        body: JSON.stringify(info)
+        body: JSON.stringify({message: 'Success'})
     }
 }
