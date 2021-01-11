cf489537d1e67f8cdaf6e4a8badb6d71b4b3c070
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2021-01-11 17:09:13 -0600

    Video 45. Created a hook to fetch client side data and display it in home page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+    return (
+        <div>
+            <p>Currently Slicing</p>
+        </div>
+    )
+}
+
+function HotSlices() {
+    return (
+        <div>
+            <p>Hot Slices</p>
+        </div>
+    )
+}
 
 export default function HomePage() {
+    const {slicemasters, hotSlices} = useLatestData();
     return (
-        <>
-            <p>Home Page</p>
-        </>
+        <div className="center">
+            <h1>
+                The Best Pizza Downtown!
+            </h1>
+            <p>
+                Open 11am to 11pm 24/7
+            </p>
+            <div>
+                <CurrentlySlicing slicemasters={slicemasters} />
+                <HotSlices hotSlices={hotSlices} />
+            </div>
+        </div>
     );
 }
---
+++ gatsby/src/utils/useLatestData.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+export default function useLatestData() {
+    // hot slices
+    const [hotSlices, setHotSlices] = useState();
+
+    // slice masters
+    const [slicemasters, setSlicemasters] = useState();
+
+    useEffect(function() {
+        // when component loads, fetch the data
+        fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `query {
+                            StoreSettings(id: "downtown") {
+                            name
+                            slicemaster {
+                                name
+                            }
+                            hotSlices {
+                                name
+                            }
+                            }
+                        }`
+            })
+        }).then(res => res.json()).then(res => {
+            // Check for errors
+            // set the data to state
+            setHotSlices(res.data.StoreSettings.hotSlices);
+            setSlicemasters(res.data.StoreSettings.slicemaster)
+        });
+    }, []);
+
+    return {
+        hotSlices,
+        slicemasters
+    }
+}
